3ab4dc8f65710fd7ca915edddba4931b288f751a
Tai Lee <real.human@mrmachine.net>
2015-04-20 17:14:39 +1000

    Add a default AppConfig so we can assign a nicer verbose name.

--- model_settings/__init__.py
+++ model_settings/__init__.py
@@ -1,1 +1,3 @@
 VERSION = '0.1'
+
+default_app_config = 'model_settings.apps.AppConfig'
---
+++ model_settings/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppConfig(AppConfig):
+    name = 'model_settings'
+    verbose_name = 'Model Settings'
