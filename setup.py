13dd4a43275f7ee6b9dd615701356ce7850e96ef
Tai Lee <real.human@mrmachine.net>
2015-10-26 00:02:07 +1100

    Updates for distribution on package index.
    * Ignore dist files.
    * Build universal wheels for distribution.
    * Use Markdown for README.
    * Convert `README.md` to reStructuredText to use as long description.
    * Do not import app package.
    * Add required metadata.
    * Include all files as package data.

--- setup.py
+++ setup.py
@@ -1,12 +1,36 @@
-from model_settings import VERSION
+from __future__ import print_function
+
 import setuptools
+import sys
+
+version = '0.1.dev0'
+
+# Convert README.md to reStructuredText.
+if {'bdist_wheel', 'sdist'}.intersection(sys.argv):
+    try:
+        import pypandoc
+    except ImportError:
+        print('ERROR: You must install `pypandoc` to convert `README.md` to '
+              'reStructuredText to use as long description.',
+              file=sys.stderr)
+        exit(1)
+    print('Converting `README.md` to reStructuredText to use as long '
+          'description.')
+    long_description = pypandoc.convert('README.md', 'rst')
 
 setuptools.setup(
     name='django-model-settings',
-    version=VERSION,
+    version=version,
+    author='Interaction Consortium',
+    author_email='studio@interaction.net.au',
+    url='https://github.com/ixc/django-model-settings',
+    description='Editable settings via polymorphic models with a dict-like '
+                'interface and template tags.',
+    long_description=locals().get('long_description', ''),
+    license='MIT',
     packages=setuptools.find_packages(),
     install_requires=[
-    	'django-classy-tags',
+        'django-classy-tags',
         'django-polymorphic',
     ]
 )
