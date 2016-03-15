f19698b0c8e9d985e797ddc3c12cf8cb603b6594
Tai Lee <real.human@mrmachine.net>
2016-03-15 13:15:19 +1100

    Fix indentation.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     use_scm_version={'version_scheme': 'post-release'},
     packages=setuptools.find_packages(),
     install_requires=[
-    	'django-classy-tags',
+        'django-classy-tags',
         'django-polymorphic',
     ],
     setup_requires=['setuptools_scm'],
