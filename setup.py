722f949295156d0bf8c26770f30fb2ab3e185b90
Faruk Akgul <faakgul@gmail.com>
2016-08-18 16:03:35 +1000

    unit tests settings for setup

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@ import setuptools
 setuptools.setup(
     name='django-model-settings',
     use_scm_version={'version_scheme': 'post-release'},
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     install_requires=[
         'django-classy-tags',
         'django-polymorphic',
     ],
     setup_requires=['setuptools_scm'],
+    extras_require={
+      "test": ["coverage==4.2"]
+    }
 )
